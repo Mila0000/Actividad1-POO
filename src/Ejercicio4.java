d621902bb1122f0b6541cb7470b2fa4ed7d96aae
Maria Camila Agudelo <96594569+Mila0000@users.noreply.github.com>
2023-03-04 15:17:56 -0500

    Initial commit

---
+++ src/Ejercicio4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int edad_juan;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ingrese la edad de Juan: ");
+        edad_juan = entrada.nextInt();
+
+        System.out.println("La edad de Juan es: " + edad_juan + " años\nLa edad de Alberto es: " + ((2*edad_juan)/3) + " años\nLa edad de Ana es: " + ((4*edad_juan)/3) + " años\nLa edad de la mamá es: " + (((2*edad_juan)/3+((4*edad_juan)/3)+edad_juan))+ " años");
+
+    }
+
+}
